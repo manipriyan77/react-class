6f802533db6f9adb805fb101065528090411d635
manipriyan77 <manipriyangopalan@gmail.com>
2022-07-21 07:46:40 +0530

    second changes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("Componenetdidmont");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
@@ -19,8 +21,25 @@ class App extends Component {
       });
   }
   render() {
+    console.log("Render");
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="Search monsters"
+          className="search-box"
+          onChange={(event) => {
+            console.log({ Starttingarray: this.state.monsters });
+
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
